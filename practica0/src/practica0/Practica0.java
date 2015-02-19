da9bd03740e95a4eb79203846a2b4b7014cb2079
Usuario <Usuario@172.20.100.37>
2015-02-19 13:25:29 +0100

    Branch test

--- practica0/src/practica0/Practica0.java
+++ practica0/src/practica0/Practica0.java
@@ -24,7 +24,7 @@ public class Practica0 {
             a
                     a
                     a
-                            a
+                            aasdfadsfa
                             a
                                     a
                                     a
